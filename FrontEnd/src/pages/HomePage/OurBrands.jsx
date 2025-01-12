94c822c48f2d48f16bf4d253bad654cec5f4e65a
Abhay Dixit <dixitshivam534@gmail.com>
2025-01-12 11:22:02 +0530

    frontend design complete

--- FrontEnd/src/pages/HomePage/OurBrands.jsx
+++ FrontEnd/src/pages/HomePage/OurBrands.jsx
@@ -21,26 +21,66 @@ const OurBrands = () => {
     </Row>
     <Row>
         <Col>
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item style={{margin:"auto"}}>
-        <img src={brand1} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand2} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand3} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand4} alt="" style={{padding:"0px 40px"}}/>
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
+        <Carousel activeIndex={index} onSelect={handleSelect} className="d-flex justify-content-between" >
+      <Carousel.Item style={{padding:"50px 0px",display:"flex",justifyContent:"space-between",alignItems:"center"}} >
+        <img
+          className="d-inline"
+          src={brand1}
+          alt="Second slide"
+          width={"100px"}
+          style={{padding:"0px 15px"}}
+        />
+        <img
+        className="d-inline"
+        src={brand2}
+        alt="Second slide"
+        width={"100px"}
+        style={{padding:"0px 15px"}}
+      />
+      <img
+      className="d-inline"
+      src={brand3}
+      alt="Second slide"
+      width={"100px"}
+      style={{padding:"0px 15px"}}
+    />
+    <img
+    className="d-inline"
+    src={brand4}
+    alt="Second slide"
+    width={"100px"}
+    style={{padding:"0px 15px"}}
+  />
       </Carousel.Item>
-      <Carousel.Item style={{margin:"auto"}}>
-      <img src={brand4} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand3} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand2} alt="" style={{padding:"0px 40px"}}/>
-        <img src={brand1} alt="" style={{padding:"0px 40px"}}/>
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+      <Carousel.Item style={{padding:"50px 0px",display:"flex",justifyContent:"space-between",alignItems:"center"}} >
+        <img
+          className="d-inline"
+          src={brand1}
+          alt="Second slide"
+          width={"100px"}
+          style={{padding:"0px 15px"}}
+        />
+        <img
+        className="d-inline"
+        src={brand2}
+        alt="Second slide"
+        width={"100px"}
+        style={{padding:"0px 15px"}}
+      />
+      <img
+      className="d-inline"
+      src={brand3}
+      alt="Second slide"
+      width={"100px"}
+      style={{padding:"0px 15px"}}
+    />
+    <img
+    className="d-inline"
+    src={brand4}
+    alt="Second slide"
+    width={"100px"}
+    style={{padding:"0px 15px"}}
+  />
       </Carousel.Item>
       
     </Carousel>
--- FrontEnd/src/pages/TermsAndConditions.jsx
+++ FrontEnd/src/pages/TermsAndConditions.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Container,Row,Col } from 'react-bootstrap'
 
 const TermsAndConditions = () => {
   return (
-    <div>TermsAndConditions</div>
+  <>
+  <Container>
+    <Row>
+       <Col>
+       <h1 style={{fontFamily: 'Libre Baskerville, serif',fontSize: '34px',fontWeight: 500,textTransform: 'none',lineHeight: 1.8,letterSpacing: 'normal',textAlign:"center"}}>Terms and Conditions</h1>
+       
+       </Col>
+    </Row>
+  </Container>
+  </>
   )
 }
 
